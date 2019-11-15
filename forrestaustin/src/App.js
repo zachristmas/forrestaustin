09952a96f8ef45da502dd061da78c3d8a55cd6da
Zachary Christmas <zachristmas@yahoo.com>
2019-11-14 20:35:36 -0500

    image fix

--- forrestaustin/src/App.js
+++ forrestaustin/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Image from './photo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='photo.jpeg' className="App-logo" alt="logo" />
+        <img src={Image} className="App-logo" alt="logo" />
         <p>
           Forrest is the greatest
           -Zach
