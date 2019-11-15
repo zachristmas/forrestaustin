6cbacacc9bf8914478309aa73391c2f36d773885
= <=>
2019-11-14 20:07:49 -0500

    next one

--- forrestaustin/src/App.js
+++ forrestaustin/src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://raw.githubusercontent.com/zachristmas/forrestaustin/master/forrestaustin/public/photo.jpeg?token=AGJKO4INCQMWETNTSWS5HDK5ZX4YK' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Forrest is the greatest
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Stuff
         </a>
       </header>
     </div>
