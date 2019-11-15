926c6018a021fd3d89fd7e8ef395e3c24fd2a62b
= <=>
2019-11-14 20:20:58 -0500

    kkk

--- forrestaustin/src/App.js
+++ forrestaustin/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://raw.githubusercontent.com/zachristmas/forrestaustin/master/forrestaustin/public/photo.jpeg?token=AGJKO4INCQMWETNTSWS5HDK5ZX4YK' className="App-logo" alt="logo" />
+        <img src='https://raw.githubusercontent.com/zachristmas/forrestaustin/master/forrestaustin/public/photo.jpeg' className="App-logo" alt="logo" />
         <p>
           Forrest is the greatest
           -Zach
